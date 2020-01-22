4b4653b551b6acc0792bfe6593514c4d17607b16
Davide <davide.moro@barsanti.edu.it>
2020-01-22 11:04:00 +0100

    Continuazione
    22/01/2020

--- codiceMorse_Moro/codiceMorse_Moro.ino
+++ codiceMorse_Moro/codiceMorse_Moro.ino
@@ -1,10 +1,18 @@
 
-#define sensore A5
-#define led 13
-int luminosità;
-int inizio;
+#include <SPI.h>
+#include <SD.h>
 
-String s;
+struct nodo {
+  String car;
+  struct nodo *tipo
+}
+
+#define sensore A5 //pin del sensore di luminosità
+#define led 13 //pin del led
+int luminosità;
+int inizio;//variabile per il calcolo del tempo 
+int fine;//variabile per il calcolo del tempo
+String app; //stringa di appoggio
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,26 +21,42 @@ void setup() {
                    
   String alfabeto[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
                        "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "."}; //alfabeto
+                       
   pinMode(led, OUTPUT);
   pinMode(sensore, INPUT);
   Serial.begin(9600);
   luminosità = 0;
-  s = "";
-  inizio = 0;
-  
-
+  app = "";
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- if
 
+  app = "";
 }
 
 void ricevere() {
   luminosità = analogRead(A5);
-  if(luminosità >= 500 {
-    inizio = millis()
-    if(digitalRead(led) == LOW && inizio)
+  if(luminosità >= 500) {
+    if(digitalRead(led) == HIGH) {
+      inizio = millis()
+    }
+    else if(digitalRead(led) == LOW) {
+      fine = millis();
+    }
+  }
+  if(fine - inizio <= 550 && fine - inizio >= 450) {
+    app = app + "."
+  }
+  else if(fine - inizio <= 1050 && fine - inizio >= 950) {
+    app = app + "-"
+  }
+  else if(fine - inizio <= 2050 && fine - inizio >= 1950) {
+    for(int i = 0; i < app.length() - 1; i++) {
+      if(app == codice[i])
+      {
+        
+      }
+    }
   } 
 }
